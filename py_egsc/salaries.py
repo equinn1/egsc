a399b63585b979c5b5bbb530b3a2e713bb417187
Eugene Quinn <gquinn@localhost.localdomain>
2020-12-23 11:35:48 -0500

    salaries

--- py_egsc/salaries.py
+++ py_egsc/salaries.py
@@ -3,13 +3,13 @@ import numpy as np
 import pandas as pd
     
 class Teacher_salary():
-    def __init__(self):                                       #constructor
+    def __init__(self):         #constructor
 
         self.cba_cols ={'B': 0,'B+30': 1,'M': 2,'M+30': 3,'M2/CAGS': 4, 'D': 5}
         
         self.cba = {}
         
-                                                       #start with FY2016 (2015-2016) salary matrix
+                                #start with FY2016 (2015-2016) salary matrix
         self.cba['2015-2016'] = np.array([
             [41286, 42900, 43871, 44505, 44893, 45186],
             [44871, 46484, 47454, 48085, 48474, 48771],
@@ -22,17 +22,16 @@ class Teacher_salary():
             [71741, 73353, 74323, 74954, 75345, 75639],
             [78898, 80675, 81743, 82438, 82866, 83190]]) 
         
-                                                       #FY2017 (2016-2017) is the same as FY2016
+                                #FY2017 (2016-2017) is the same as FY2016
         self.cba['2016-2017'] = self.cba['2015-2016']
         
-                                                       #FY2018 (2017-2018) 2% increase
+                                #FY2018 (2017-2018) 2% increase
         self.cba['2017-2018'] = np.around(1.02*self.cba['2016-2017'],0)
         
-                                                       #FY2019 (2018-2019) 2.25% increase
+                                #FY2019 (2018-2019) 2.25% increase
         self.cba['2018-2019'] = np.around(1.0225*self.cba['2017-2018'],0)
         
-                                                       #FY2020 (2019-2020) same as FY2019
-
+                                #FY2020 (2019-2020) same as FY2019
         self.cba['2019-2010'] = np.array([
             [43059, 44743, 45755, 46416, 46821, 47127],
             [46798, 48480, 49492, 50150, 50556, 50866],
@@ -45,7 +44,7 @@ class Teacher_salary():
             [74822, 76504, 77515, 78173, 78581, 78888],
             [82287, 84140, 85254, 85979, 86425, 86763]]) 
         
-                                                       #FY2021 (2020-2021) 2% increase
+                                #FY2021 (2020-2021) 2% increase, don't round to dollar
         self.cba['2020-2021'] = np.array([
             [43920.18, 45637.86, 46670.10, 47344.32, 47757.42, 48069.54],
             [47733.96, 49449.60, 50481.84, 51153.00, 51567.12, 51883.32],
@@ -58,7 +57,7 @@ class Teacher_salary():
             [76318.44, 78034.08, 79065.30, 79736.46, 80152.62, 80465.76],
             [83932.74, 85822.80, 86959.08, 87698.58, 88153.50, 88498.26]]) 
         
-                                                       #FY2022 (2021-2022) 2.25% increase
+                                #FY2022 (2021-2022) 2.25% increase, don't round to dollar
         self.cba['2021-2022'] = np.array([
             [44908.38, 46664.71, 47720.18, 48409.57, 48831.96, 49151.10],
             [48807.97, 50562.22, 51617.68, 52303.94, 52727.38, 53050.69],
@@ -71,26 +70,25 @@ class Teacher_salary():
             [78035.60, 79789.85, 80844.27, 81530.53, 81956.05, 82276.24],
             [85821.23, 87753.81, 88915.66, 89671.80, 90136.95, 90489.47]]) 
         
-                                                                #FY2023 (2022-2023) same as FY2022
+                                #FY2023 (2022-2023) same as FY2022
         self.cba['2022-2023'] = self.cba['2021-2022']
         
-                                                                #FY2024 (2023-2024) 2% increase
-        self.cba['2023-2024'] = 1.02*self.cba['2022-2023']
+                                #FY2024 (2023-2024) 2% increase, don't round to dollar
+        self.cba['2023-2024'] = np.around(1.02*self.cba['2022-2023'],2)
         
-                                                                #FY2025 (2024-2025) 2.25% increase
-        self.cba['2024-2025'] = 1.0225*self.cba['2023-2024']
-
+                                #FY2025 (2024-2025) 2.25% increase, don't round to dollar
+        self.cba['2024-2025'] = np.around(1.0225*self.cba['2023-2024'],2)
         
-                                                          #FY2015: back out 2.5% increase from FY2016
+                                #FY2015: back out 2.5% increase from FY2016, round to dollar
         self.cba['2014-2015'] = np.around(self.cba['2015-2016']/1.025,0) 
         
-                                                          #FY2014: back out 2% increase from FY2015
+                                #FY2014: back out 2% increase from FY2015, round to dollar
         self.cba['2013-2014'] = np.around(self.cba['2014-2015']/1.02,0)  
         
-                                                       #FY2013: back out 1.01% from FY2014 for steps 1-9
+                                #FY2013: back out 1.01% from FY2014 for steps 1-9, round to dollar
         self.cba['2012-2013'] = self.cba['2013-2014']
         self.cba['2012-2013'][0:8,:] = np.around(self.cba['2012-2013'][0:8,:]/1.01,0)
-                                                       #FY2013: back out 2.25% from FY2014 for step 10
+                                #FY2013: back out 2.25% from FY2014 for step 10, round to dollar
         self.cba['2012-2013'][9,:]   = np.around(self.cba['2012-2013'][9,:]/1.0225,0)  
         return            
     
